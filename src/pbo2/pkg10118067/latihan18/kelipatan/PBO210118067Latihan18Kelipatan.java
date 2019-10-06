db4eef7ff5c6f436bd0d57fb542396f5011b7743
Agus Awaludin <55693286+AgusAwaludinUnikom@users.noreply.github.com>
2019-10-06 09:28:21 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118067/latihan18/kelipatan/PBO210118067Latihan18Kelipatan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118067.latihan18.kelipatan;
+
+/**
+ *
+ *  Agus Awaludin
+ *  IF2
+ *  10118067
+ *  Latihan ini besisi tentang bagaimana caranya menampilkan kelipatan
+ */
+public class PBO210118067Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    int i;
+    i=1;
+    
+    do{
+        System.out.println(+i*3.5);
+        i++;
+                
+    }while(i<=10);
+    }
+    
+}
